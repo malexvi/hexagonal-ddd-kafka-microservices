75a69a0aaeb6e780c63dd8d7d6cfd0350c62c8b6
Marcos <m.alex.vi.silva@gmail.com>
2026-02-21 09:02:25 -0400

    feat: Finishes order-domain-core, implements OrderDomainServiceImpl, add dependencies for logs in the base project

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/OrderDomainService.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/OrderDomainService.java
@@ -2,6 +2,7 @@ package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.entity.Restaurant;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
 import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
 import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
 
@@ -15,7 +16,7 @@ public interface OrderDomainService {
 
     void approveOrder(Order order);
 
-    void cancelOrderPayment(Order order, List<String> failureMessages);
+    OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages);
 
     void cancelOrder(Order order, List<String> failureMessages);
 }

69933c281e03c78111e83a5d3c18d96a9a848a7d
Marcos <m.alex.vi.silva@gmail.com>
2026-02-21 11:26:33 -0400

    feat: Create order-application-service dtos

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/OrderItem.java
@@ -0,0 +1,24 @@
+package com.food.ordering.system.order.service.domain.dto.create;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class OrderItem {
+
+    @NotNull
+    private final UUID productId;
+    @NotNull
+    private Integer quantity;
+    @NotNull
+    private final BigDecimal prirce;
+    @NotNull
+    private final BigDecimal subTotal;
+}
---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/track/TrackOrderResponse.java
@@ -0,0 +1,21 @@
+package com.food.ordering.system.order.service.domain.dto.track;
+
+import com.food.ordering.system.domain.valueobject.OrderStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.hibernate.validator.constraints.UUID;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class TrackOrderResponse {
+    @NotNull
+    private final UUID orderTranckingId;
+    @NotNull
+    private final OrderStatus orderStatus;
+    private final List<String> failureMessages;
+}
